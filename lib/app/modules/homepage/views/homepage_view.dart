6908c80c8304afdf7d51ec7978801bdcece60a9a
shawo <shawoncse954@gmail.com>
2023-09-15 12:16:48 +0600

    My Portfolio

--- lib/app/modules/homepage/views/homepage_view.dart
+++ lib/app/modules/homepage/views/homepage_view.dart
@@ -2,22 +2,19 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../coustom_widget/backround_screen/backround_screen.dart';
+import '../../../coustom_widget/coustom_appbar/coustom_app_bar.dart';
 import '../controllers/homepage_controller.dart';
 
 class HomepageView extends GetView<HomepageController> {
   const HomepageView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomepageView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomepageView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+    return BackroundScreen(
+      body: Column(
+        children: [
+          CustomAppBar(),
+        ],
       ),
     );
   }
