7881094b5625522b56952365bfcc6319d8bda52d
shawo <shawoncse954@gmail.com>
2023-09-15 10:22:21 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ar_rahman/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ar_rahman
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:ar_rahman/app/routes/app_pages.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+import 'app/modules/homepage/bindings/homepage_binding.dart';
+
+Future <void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
+  runApp(
+    ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context,child){
+        SystemChrome.setPreferredOrientations([
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.portraitDown,
+        ]);
+        return GetMaterialApp(
+          title: 'AR RAHMAN',
+          debugShowCheckedModeBanner: false,
+          initialRoute: '/homepage',
+          initialBinding: HomepageBinding(),
+          getPages: AppPages.routes,
+        );
+      },
+    ),
+  );
+}
+
+
