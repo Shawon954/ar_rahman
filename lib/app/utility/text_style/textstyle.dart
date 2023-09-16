0b23f1f7f8d0bb103c30b435f787625e7cd8b65c
shawo <shawoncse954@gmail.com>
2023-09-16 15:12:30 +0600

    My Portfolio

---
+++ lib/app/utility/text_style/textstyle.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+
+import 'package:flutter/src/painting/text_style.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class googlefonts {
+  TextStyle textStyle20black = GoogleFonts.inter(
+    fontSize: 10.sp,
+    color: Color(0xd2111010),
+    fontWeight: FontWeight.bold,
+  );
+  TextStyle textStyle16blacknavbar = GoogleFonts.inter(
+      fontSize: 4.sp,
+      color: Color(0xd2111010),
+      fontWeight: FontWeight.w500,
+      letterSpacing: 1.1);
+  TextStyle textStyle16black = GoogleFonts.inter(
+      fontSize: 5.sp,
+      color: Color(0xd2111010),
+      fontWeight: FontWeight.w700,
+      letterSpacing: 1.1);
+  TextStyle textStyle16blacknormal = GoogleFonts.inter(
+      fontSize: 5.sp,
+      color: Color(0xd2111010),
+      fontWeight: FontWeight.normal,
+      letterSpacing: 1.0);
+  TextStyle textStyle14black = GoogleFonts.inter(
+      fontSize: 5.sp,
+      color: Color(0xd2111010),
+      fontWeight: FontWeight.w400,
+      letterSpacing: 1.1,
+      fontStyle: FontStyle.italic);
+}
