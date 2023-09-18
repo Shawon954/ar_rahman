9f51e5b644d315ae915954c9c812f7610642dda6
shawo <shawoncse954@gmail.com>
2023-09-18 22:27:52 +0600

    Contact me Button add

--- lib/app/modules/homepage/views/homepage_view.dart
+++ lib/app/modules/homepage/views/homepage_view.dart
@@ -98,7 +98,7 @@ class HomepageView extends GetView<HomepageController> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text("Hello Sona",style: TextStyle(color: Colors.red),),
+
                   bodyOne(),
                   bodyTwo(),
 
