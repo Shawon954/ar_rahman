d7539f812505c5ac7dfe4909d3348fad632e63cf
shawo <shawoncse954@gmail.com>
2023-09-18 22:24:55 +0600

    Contact me Button add

--- lib/app/modules/homepage/controllers/homepage_controller.dart
+++ lib/app/modules/homepage/controllers/homepage_controller.dart
@@ -1,9 +1,48 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class HomepageController extends GetxController {
-  //TODO: Implement HomepageController
+import '../../../coustom_widget/coustom_appbar/navbar.dart';
 
-  final count = 0.obs;
+class HomepageController extends GetxController {
+
+ final  collapsableHeight = 0.0.obs;
+
+  Color  selected = Color(0xffffffff);
+  Color notSelected = Color(0xafffffff);
+
+  List<Widget> navBarItems = [
+    NavBarItem(
+      text: 'Home',
+    ),
+    NavBarItem(
+      text: 'About',
+    ),
+    NavBarItem(
+      text: 'Skills',
+    ),
+    NavBarItem(
+      text: 'My Work',
+    ),
+    NavBarItem(
+      text: 'My Work',
+    ),
+    NavBarItem(
+      text: 'Contact',
+    ),
+  ];
+
+
+   screensize(){
+       if (collapsableHeight.value == 0.0) {
+           collapsableHeight.value = 240;
+       } else if (collapsableHeight.value == 240.0) {
+         collapsableHeight.value = 0;
+
+     };
+  }
+
+
   @override
   void onInit() {
     super.onInit();
@@ -16,8 +55,9 @@ class HomepageController extends GetxController {
 
   @override
   void onClose() {
+
     super.onClose();
   }
 
-  void increment() => count.value++;
+
 }
