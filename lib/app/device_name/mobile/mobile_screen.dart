902763d1aecd63d1cf3a10ed850c6ca82a58a5fd
shawo <shawoncse954@gmail.com>
2023-09-20 23:37:45 +0600

    Contact me Button add

---
+++ lib/app/device_name/mobile/mobile_screen.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class Mobile extends StatelessWidget {
+  const Mobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
