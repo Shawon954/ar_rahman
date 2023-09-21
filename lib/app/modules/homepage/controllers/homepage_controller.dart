835bfe15e1add47fa973c36c05e225e8067aca5e
shawo <shawoncse954@gmail.com>
2023-09-21 23:43:00 +0600

    Contact me Button add

--- lib/app/modules/homepage/controllers/homepage_controller.dart
+++ lib/app/modules/homepage/controllers/homepage_controller.dart
@@ -6,43 +6,10 @@ import '../../../coustom_widget/coustom_appbar/navbar.dart';
 
 class HomepageController extends GetxController {
 
- final  collapsableHeight = 0.0.obs;
-
-  Color  selected = Color(0xffffffff);
-  Color notSelected = Color(0xafffffff);
-
-  List<Widget> navBarItems = [
-    NavBarItem(
-      text: 'Home',
-    ),
-    NavBarItem(
-      text: 'About',
-    ),
-    NavBarItem(
-      text: 'Skills',
-    ),
-    NavBarItem(
-      text: 'My Work',
-    ),
-    NavBarItem(
-      text: 'My Work',
-    ),
-    NavBarItem(
-      text: 'Contact',
-    ),
-  ];
 
 
-   screensize(){
-       if (collapsableHeight.value == 0.0) {
-           collapsableHeight.value = 240;
-           navBarItems;
-       } else if (collapsableHeight.value == 240.0) {
-         collapsableHeight.value = 0;
-         navBarItems;
 
-     };
-  }
+
 
 
   @override
