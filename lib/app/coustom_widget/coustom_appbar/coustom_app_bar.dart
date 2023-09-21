edaf96d3dd580219946fe85c621ae90c3e8fa101
shawo <shawoncse954@gmail.com>
2023-09-21 23:42:51 +0600

    Contact me Button add

--- lib/app/coustom_widget/coustom_appbar/coustom_app_bar.dart
+++ lib/app/coustom_widget/coustom_appbar/coustom_app_bar.dart
@@ -1,114 +1,122 @@
-import 'package:ar_rahman/app/core/app_image/app_images.dart';
-import 'package:ar_rahman/app/utility/text_style/textstyle.dart';
-import 'package:day_night_switcher/day_night_switcher.dart';
+import 'package:ar_rahman/app/coustom_widget/coustom_appbar/button_navbar.dart';
+import 'package:ar_rahman/app/coustom_widget/coustom_appbar/navbar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter/services.dart';
 
-class CustomAppBar extends StatefulWidget {
-  const CustomAppBar({super.key});
+class AdaptiveNavBar extends AppBar {
+  /// [canTitleGetTapped]
+  final bool canTitleGetTapped;
 
-  @override
-  State<CustomAppBar> createState() => _CustomAppBarState();
-}
 
-class _CustomAppBarState extends State<CustomAppBar> {
-  late bool isDarkModeEnabled = false;
-  late bool hovor = false;
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 70.h,
-      width: MediaQuery.of(context).size.width,
-      color: Colors.orange,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Md.Abdur Rahman'),
-            AnimatedContainer(
-              duration: Duration(seconds: 2),
-              child: Row(
-                children: [
-                  InkWell(
-                    onHover: (isHovering) {
+  /// [onTitleTapped]
+  final void Function()? onTitleTapped;
 
-                      if (isHovering) {
-                        setState(() {
-                          hovor = !isHovering;
+  /// Based on [screenWidth], the 'AdaptiveNavBar' decides
+  /// which widget it should be displayed
+  ///
+  /// If `screenWidth < 700` it displays [NavBarSmall]
+  /// else it displays [NavBarWide]
+  final double? screenWidth;
 
-                        });
-                      }
-                    },
-                    child: Text(
-                      'Home',
-                      style: googlefonts().textStyle16blacknavbar,
-                    ),
-                    onTap: () {},
-                  ),
-                  SizedBox(
-                    width: 6.w,
-                  ),
-                  InkWell(
-                    child: Text(
-                      'About',
-                      style: googlefonts().textStyle16blacknavbar,
-                    ),
-                    onTap: () {},
-                  ),
-                  SizedBox(
-                    width: 6.w,
-                  ),
-                  InkWell(
-                    child: Text(
-                      'Skills',
-                      style: googlefonts().textStyle16blacknavbar,
-                    ),
-                    onTap: () {},
-                  ),
-                  SizedBox(
-                    width: 6.w,
-                  ),
-                  InkWell(
-                    child: Text(
-                      'My Work',
-                      style: googlefonts().textStyle16blacknavbar,
-                    ),
-                    onTap: () {},
-                  ),
-                  SizedBox(
-                    width: 6.w,
-                  ),
-                  InkWell(
-                    child: Text(
-                      'Contact',
-                      style: googlefonts().textStyle16blacknavbar,
-                    ),
-                    onTap: () {},
-                  ),
-                  SizedBox(
-                    width: 6.w,
-                  ),
-                  SizedBox(
-                    height: 45.h,
-                    width: 12.w,
-                    child: DayNightSwitcher(
-                      isDarkModeEnabled: isDarkModeEnabled,
-                      onStateChanged: (isDarkModeEnabled) {
-                        setState(() {
-                          this.isDarkModeEnabled = isDarkModeEnabled;
-                        });
-                      },
-                    ),
-                  ),
-                ],
+  /// [navBarItems] instead of [actions] in `AppBar`
+  ///
+  /// The list of navBarItems of type `List<NavBarItem>`
+  final List<NavBarItem> navBarItems;
+
+  /// `AdaptiveNavBar`'s named constructor
+  AdaptiveNavBar({
+    this.canTitleGetTapped = false,
+    this.onTitleTapped,
+    required this.screenWidth,
+    required this.navBarItems,
+    Key? key,
+    Widget? title,
+    Widget? leading,
+    bool automaticallyImplyLeading = true,
+    Widget? flexibleSpace,
+    PreferredSizeWidget? bottom,
+    double? elevation,
+    Color? shadowColor,
+    ShapeBorder? shape,
+    Color? backgroundColor,
+    Color? foregroundColor,
+    IconThemeData? iconTheme,
+    IconThemeData? actionsIconTheme,
+    bool primary = true,
+    bool? centerTitle,
+    bool excludeHeaderSemantics = false,
+    double? titleSpacing,
+    double toolbarOpacity = 1.0,
+    double bottomOpacity = 1.0,
+    double? toolbarHeight,
+    double? leadingWidth,
+    TextStyle? toolbarTextStyle,
+    TextStyle? titleTextStyle,
+    SystemUiOverlayStyle? systemOverlayStyle,
+  }) : super(
+    key: key,
+    title: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: canTitleGetTapped
+          ? InkWell(
+        onTap: onTitleTapped,
+        child: title ??
+            const Text(
+              "MB NavBar",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.purple
               ),
-            )
-          ],
-        ),
+            ),
+      )
+          : title ??
+          const Text(
+            "MB NavBar",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+                color: Colors.purple
+            ),
+          ),
+    ),
+
+
+
+    actions: [
+
+      screenWidth! <800.toDouble()
+          ? NavBarSmall(
+        navBarItems: navBarItems,
+
+      )
+          : NavBarWide(
+        navBarItems: navBarItems,
       ),
-    );
-  }
+
+    ],
+    leading: leading,
+    automaticallyImplyLeading: automaticallyImplyLeading,
+    flexibleSpace: flexibleSpace,
+    bottom: bottom,
+    elevation: elevation,
+    shadowColor: shadowColor,
+    shape: shape,
+    backgroundColor: backgroundColor,
+    foregroundColor: foregroundColor,
+    iconTheme: iconTheme,
+    actionsIconTheme: actionsIconTheme,
+    primary: primary,
+    centerTitle: centerTitle,
+    excludeHeaderSemantics: excludeHeaderSemantics,
+    titleSpacing: titleSpacing,
+    toolbarOpacity: toolbarOpacity,
+    bottomOpacity: bottomOpacity,
+    toolbarHeight: toolbarHeight,
+    leadingWidth: leadingWidth,
+    toolbarTextStyle: toolbarTextStyle,
+    titleTextStyle: titleTextStyle,
+    systemOverlayStyle: systemOverlayStyle,
+  );
 }
