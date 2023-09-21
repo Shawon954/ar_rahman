1e4a0c75e5981baf4d57ca8b1ca97881f6bf77cb
shawo <shawoncse954@gmail.com>
2023-09-21 23:43:31 +0600

    Contact me Button add

--- lib/app/device_name/mobile/mobile_screen.dart
+++ lib/app/device_name/mobile/mobile_screen.dart
@@ -1,11 +1,13 @@
 
 import 'package:flutter/material.dart';
 
+import '../../modules/homepage/views/homepage_view.dart';
+
 class Mobile extends StatelessWidget {
   const Mobile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return HomepageView();
   }
 }
