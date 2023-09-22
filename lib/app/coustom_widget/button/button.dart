5e975a5d44be8b5b18ae530ec26cf45b63a9f10c
shawo <shawoncse954@gmail.com>
2023-09-22 22:34:12 +0600

    Contact me Button add

--- lib/app/coustom_widget/button/button.dart
+++ lib/app/coustom_widget/button/button.dart
@@ -1,32 +1,33 @@
-import 'package:ar_rahman/app/responsive/responsive_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../utility/app_colors/appcolors.dart';
 
 class CoustomeButton extends StatelessWidget {
 
   final child;
   final onPressed;
 
+   final height;
+    final width;
+    final fontSize;
+   final textstyle;
 
 
-  const CoustomeButton({super.key, this.child, this.onPressed});
+  const CoustomeButton({super.key, this.child, this.onPressed, this.height, this.width, this.fontSize, this.textstyle,});
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.sizeOf(context).width;
+
     return SizedBox(
-      height: Responsive.isMobile(context)?width/15:width/15,
-      width:Responsive.isMobile(context)?width/5.5:width/5.5,
+      height: height,
+      width:width,
       child: ElevatedButton(
         child: child,
         onPressed: onPressed,
         style: ElevatedButton.styleFrom(
-            primary: Colors.purple,
-            textStyle: TextStyle(
-                fontSize: Responsive.isMobile(context)?width/45:width/40,
-                fontWeight: FontWeight.bold)
+          backgroundColor:AppColors.primary,
         ),
-      ),
+      )
     );
   }
 }
