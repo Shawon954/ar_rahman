10cc0cb35857b7a13e96c1eda66ba28995bb06e1
shawo <shawoncse954@gmail.com>
2023-09-25 00:34:44 +0600

    Skill Section

--- lib/app/core/app_image/app_images.dart
+++ lib/app/core/app_image/app_images.dart
@@ -4,7 +4,9 @@ class AppImage{
 
   static const String laptopimage = 'assets/lottie/laptop.json';
   static const String developimage = 'assets/lottie/development.json';
-  static const String coding = 'assets/lottie/coding_laptop.json';
+
+
+
   static const String ImageIcons = 'assets/images/facebook.png';
 
 
